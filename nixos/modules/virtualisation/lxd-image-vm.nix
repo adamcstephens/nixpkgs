69d560c7556f3c7e6f0731b009e96c639866f263
Adam Stephens <adam@valkor.net>
2022-09-04 18:59:35 +0000

    nixos/lxd-image: add VM image

---
+++ nixos/modules/virtualisation/lxd-image-vm.nix
@@ -0,0 +1,73 @@
+{ config
+, lib
+, pkgs
+, modulesPath
+, ...
+}:
+with lib; let
+  cfg = config.lxd-image-vm;
+in
+{
+  options = {
+    lxd-image-vm = {
+      vmDerivationName = mkOption {
+        type = types.str;
+        default = "nixos-lxd-${config.system.nixos.label}-${pkgs.stdenv.hostPlatform.system}";
+        description = ''
+          The name of the derivation for the LXD VM image.
+        '';
+      };
+    };
+  };
+
+  imports = [
+    ../profiles/qemu-guest.nix
+    ./lxd-agent.nix
+  ];
+
+  config = {
+    system.build.qemuImage = import ../../lib/make-disk-image.nix {
+      name = cfg.vmDerivationName;
+
+      inherit pkgs lib config;
+
+      partitionTableType = "efi";
+      format = "qcow2";
+      copyChannel = false;
+    };
+
+    fileSystems = {
+      "/" = {
+        device = "/dev/disk/by-label/nixos";
+        autoResize = true;
+        fsType = "ext4";
+      };
+      "/boot" = {
+        device = "/dev/disk/by-label/ESP";
+        fsType = "vfat";
+      };
+    };
+
+    boot.growPartition = true;
+    boot.loader.systemd-boot.enable = true;
+    boot.kernelParams = [ "console=tty1" "console=ttyS0" ];
+    systemd.services."serial-getty@ttyS0" = {
+      enable = true;
+      wantedBy = [ "getty.target" ];
+      serviceConfig.Restart = "always";
+    };
+
+    # swapDevices = [
+    #   {
+    #     device = "/var/swap";
+    #     size = 2048;
+    #   }
+    # ];
+
+    networking.useDHCP = true;
+
+    documentation.nixos.enable = false;
+    documentation.enable = false;
+    programs.command-not-found.enable = false;
+  };
+}
