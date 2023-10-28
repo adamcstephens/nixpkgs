81195487b77d21feff3bd40e9aafaa127118ee52
Adam Stephens <adam@valkor.net>
2023-10-28 00:35:57 -0400

    atomix: merged branch atomix-unstable

--- pkgs/tools/virtualization/distrobuilder/default.nix
+++ pkgs/tools/virtualization/distrobuilder/default.nix
@@ -8,7 +8,7 @@
 , gnutar
 , squashfsTools
 , debootstrap
-, fetchpatch
+, callPackage
 }:
 
 let
@@ -22,34 +22,21 @@ let
 in
 buildGoModule rec {
   pname = "distrobuilder";
-  version = "2.1";
+  version = "unstable-2023-10-26";
 
-  vendorHash = "sha256-yRMsf8KfpNmVUX4Rn4ZPLUPFZCT/g78MKAfgbFDPVkE=";
+  vendorHash = "sha256-rSu2MA7EFFOIV4wvrnM+vn80V5OSLtai6CUKHd27c+g=";
 
   src = fetchFromGitHub {
     owner = "lxc";
     repo = "distrobuilder";
-    rev = "distrobuilder-${version}";
-    sha256 = "sha256-t3ECLtb0tvIzTWgjmVQDFza+kcm3abTZZMSGYjvw1qQ=";
+    rev = "fb95dd42b3b5e0f3763032eecc3c1300ec5a445c";
+    sha256 = "sha256-j1xWF7veVQYoMyg8jywB5NR6IkB2OhjNCEitxBGziJY=";
     fetchSubmodules = false;
   };
 
   buildInputs = bins;
 
-  patches = [
-    # go.mod update: needed to to include a newer lxd which contains
-    # https://github.com/canonical/lxd/commit/d83f061a21f509d42b7a334b97403d2a019a7b52
-    # which is needed to fix the build w/glibc-2.36.
-    (fetchpatch {
-      url = "https://github.com/lxc/distrobuilder/commit/5346bcc77dd7f141a36a8da851f016d0b929835e.patch";
-      sha256 = "sha256-H6cSbY0v/FThx72AvoAvUCs2VCYN/PQ0W4H82mQQ3SI=";
-    })
-    # Fixup to keep it building after go.mod update.
-    (fetchpatch {
-      url = "https://github.com/lxc/distrobuilder/commit/2c8cbfbf603e7446efce9f30812812336ccf4f2c.patch";
-      sha256 = "sha256-qqofghcHGosR2qycGb02c8rwErFyRRhsRKdQfyah8Ds=";
-    })
-  ];
+  passthru.generator = callPackage ./generator.nix { inherit src version; };
 
   # tests require a local keyserver (mkg20001/nixpkgs branch distrobuilder-with-tests) but gpg is currently broken in tests
   doCheck = false;
