ba0c3db8150205bb8e7351cb3bfca226b40f09b0
Adam Stephens <adam@valkor.net>
2024-01-13 09:04:49 -0500

    atomix: applied PR 280739

--- nixos/modules/services/networking/soju.nix
+++ nixos/modules/services/networking/soju.nix
@@ -11,7 +11,8 @@ let
   logCfg = optionalString cfg.enableMessageLogging
     "log fs ${stateDir}/logs";
 
-  configFile = pkgs.writeText "soju.conf" ''
+  configFile = "/etc/soju.conf";
+  configFileUnsubstituted = pkgs.writeText "soju.conf" ''
     ${listenCfg}
     hostname ${cfg.hostName}
     ${tlsCfg}
@@ -58,6 +59,14 @@ in
       default = null;
       example = "/var/host.key";
       description = lib.mdDoc "Path to server TLS certificate key.";
+    };
+
+    environmentFile = lib.mkOption {
+      type = lib.types.nullOr lib.types.path;
+      default = null;
+      description = lib.mdDoc ''
+        File containing environment variables to be substituted into the final configuration.
+      '';
     };
 
     enableMessageLogging = mkOption {
@@ -111,8 +120,20 @@ in
       description = "soju IRC bouncer";
       wantedBy = [ "multi-user.target" ];
       after = [ "network-online.target" ];
+      preStart = ''
+        # substitute the settings file by environment variables
+        # in this case read from EnvironmentFile
+        test -f '${configFile}' && rm -f '${configFile}'
+        old_umask=$(umask)
+        umask 0177
+        ${pkgs.envsubst}/bin/envsubst \
+          -o '${configFile}' \
+          -i '${configFileUnsubstituted}'
+        umask $old_umask
+'';
       serviceConfig = {
         DynamicUser = true;
+        EnvironmentFile = cfg.environmentFile;
         Restart = "always";
         ExecStart = "${pkgs.soju}/bin/soju -config ${configFile}";
         StateDirectory = "soju";
