17039736e5bd269703590bb81943e7c9d77d26ea
Adam C. Stephens <adam@valkor.net>
2025-08-01 15:58:39 -0400

    kanidm: use package version in tests
    (cherry picked from commit 9b6cad46ea7f139329bc067a1abfc7772b841557)

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -693,8 +693,18 @@ in
   k3s = handleTest ./k3s { };
   kafka = handleTest ./kafka { };
   kanboard = runTest ./web-apps/kanboard.nix;
-  kanidm = handleTest ./kanidm.nix { };
-  kanidm-provisioning = handleTest ./kanidm-provisioning.nix { };
+  kanidm =
+    kanidmVersion:
+    runTest {
+      imports = [ ./kanidm.nix ];
+      _module.args = { inherit kanidmVersion; };
+    };
+  kanidm-provisioning =
+    kanidmVersion:
+    runTest {
+      imports = [ ./kanidm-provisioning.nix ];
+      _module.args = { inherit kanidmVersion; };
+    };
   karma = handleTest ./karma.nix { };
   kavita = handleTest ./kavita.nix { };
   kbd-setfont-decompress = handleTest ./kbd-setfont-decompress.nix { };
