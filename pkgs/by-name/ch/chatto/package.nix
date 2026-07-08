eda07bd42ffa56cfbe6ae1604d2c12745a724f08
Adam C. Stephens <adam@valkor.net>
2026-07-08 15:33:18 -0400

    chatto: init at 0.4.3
    Changelog: https://github.com/chattocorp/chatto/blob/refs/tags/v0.4.3/CHANGELOG.md

---
+++ pkgs/by-name/ch/chatto/package.nix
@@ -0,0 +1,105 @@
+{
+  buildGoModule,
+  fetchFromGitHub,
+  fetchPnpmDeps,
+  lib,
+  nix-update-script,
+  nodejs,
+  pnpmConfigHook,
+  pnpm_10,
+  stdenvNoCC,
+  writableTmpDirAsHomeHook,
+}:
+let
+  pname = "chatto";
+  version = "0.4.3";
+
+  src = fetchFromGitHub {
+    owner = "chattocorp";
+    repo = "chatto";
+    tag = "v${version}";
+    hash = "sha256-3Q65oSgRiN124rq/tgkHF62bywiDAKoDVFI3oW0GVnc=";
+  };
+
+  web = stdenvNoCC.mkDerivation (webFinalAttrs: {
+    pname = "${pname}-web";
+    inherit src version;
+
+    pnpmDeps = fetchPnpmDeps {
+      inherit (webFinalAttrs) pname version src;
+      pnpm = pnpm_10;
+      fetcherVersion = 4;
+      hash = "sha256-M83JXptIyoC8cZ1uyLd7rddXxhzkcl1pufeqnYV+eDI=";
+    };
+
+    nativeBuildInputs = [
+      nodejs
+      pnpm_10
+      pnpmConfigHook
+      writableTmpDirAsHomeHook
+    ];
+
+    buildPhase = ''
+      runHook preBuild
+
+      pnpm --filter @chatto/api-types build
+      pnpm --filter chatto-frontend build
+
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      cp -r apps/frontend/build $out
+
+      runHook postInstall
+    '';
+  });
+in
+buildGoModule (finalAttrs: {
+  inherit pname version src;
+
+  __structuredAttrs = true;
+  strictDeps = true;
+
+  modRoot = "cli";
+
+  vendorHash = "sha256-Fif+HL2HVGdy1gdTFWVP5aEvkRtoACW1PtH8RxidddQ=";
+
+  env.CGO_ENABLED = 0;
+
+  postPatch = ''
+    install -D -m644 LICENSES/AGPL-3.0-or-later.txt cli/cmd/embedded/LICENSE
+    install -D -m644 NOTICE cli/cmd/embedded/NOTICE
+
+    rm -rf cli/internal/http_server/.client
+    cp -r ${web} cli/internal/http_server/.client
+  '';
+
+  checkFlags =
+    let
+      skippedTests = [
+        "TestPrepareOperatorAPISocket/rejects_parent_directory_with_setgid_bit" # tries to setgid
+      ];
+    in
+    [ "-skip=^${builtins.concatStringsSep "$|^" skippedTests}$" ];
+
+  passthru = {
+    inherit web;
+    updateScript = nix-update-script { };
+  };
+
+  meta = {
+    description = "A really good chat application that you can self-host";
+    homepage = "https://github.com/chattocorp/chatto";
+    changelog = "https://github.com/chattocorp/chatto/blob/${finalAttrs.src.rev}/CHANGELOG.md";
+    license = with lib.licenses; [
+      agpl3Plus
+      asl20
+    ];
+    maintainers = with lib.maintainers; [ ];
+    mainProgram = "chatto";
+    platforms = lib.platforms.all;
+  };
+})
